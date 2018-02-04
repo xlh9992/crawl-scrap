587ab4a5897271feda08df88357092dab5fd6183
xlh9992 <xlh9992@gmail.com>
2018-02-04 15:32:11 +0000

    ‘added’

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from selenium import webdriver
-browser = webdriver.PhantomJS()  # DO NOT FORGET to set path
-browser.get("https://www.google.co.jp/") # enables to access page
-print(browser.title)
+driver = webdriver.PhantomJS()  # DO NOT FORGET to set path
+driver.get("https://www.google.co.jp/") # enables to access page
+print(driver.title)
